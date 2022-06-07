7d76d23aeed190c9a9701a343e445053f33ed324
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-06-06 21:43:25 -0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,3 +1,16 @@
-function closeMenu() {
-    
+let menuButton = document.querySelector('#menu-button');
+let sidebar = document.querySelector('.sidebar');
+let searchButton = document.querySelector('.search-button');
+let logoutButton = document.querySelector('.log-out');
+
+menuButton.onclick = function() {
+    sidebar.classList.toggle('active');
+}
+
+searchButton.onclick = function() {
+    sidebar.classList.toggle('active');
+}
+
+logoutButton.onclick = function() {
+    sidebar.classList.toggle('active');
 }
