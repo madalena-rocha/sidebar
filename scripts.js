0062a933b4f4d8901ec80a193d936a0bc6b39b80
Madalena Machado Rocha <rochamada1997m@gmail.com>
2022-06-10 11:40:18 -0300

    Update scripts.js
    Melhorando o código.

--- scripts.js
+++ scripts.js
@@ -1,16 +1,5 @@
-let menuButton = document.querySelector('#menu-button');
 let sidebar = document.querySelector('.sidebar');
-let searchButton = document.querySelector('.search-button');
-let logoutButton = document.querySelector('.log-out');
 
-menuButton.onclick = function() {
-    sidebar.classList.toggle('active');
-}
-
-searchButton.onclick = function() {
-    sidebar.classList.toggle('active');
-}
-
-logoutButton.onclick = function() {
+function openCloseSidebar() {
     sidebar.classList.toggle('active');
 }
